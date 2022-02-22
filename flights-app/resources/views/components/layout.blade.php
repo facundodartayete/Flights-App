632ab32fa8fddc2b171ba30a905a56768780e1f5
Facundo <facundodartayete@gmail.com>
2022-02-22 16:17:08 -0300

    vue install

--- flights-app/resources/views/components/layout.blade.php
+++ flights-app/resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <style>
     html {
@@ -20,11 +22,15 @@
     .clamp.one-line {
         -webkit-line-clamp: 1;
     }
+
 </style>
+
 <body style="font-family: Open Sans, sans-serif">
-    <section class="px-6 py-8">
+    <div id="app">
+        <section class="px-6 py-8">
 
-        {{ $slot }}
+            {{ $slot }}
 
-    </section>
+        </section>
+    </div>
 </body>
