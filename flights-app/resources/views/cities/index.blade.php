e9fa90a9159a7d4565fa8908e398b384a591bea7
Facundo <facundodartayete@gmail.com>
2022-02-23 10:22:14 -0300

    ajax requests on blade frontend

--- flights-app/resources/views/cities/index.blade.php
+++ flights-app/resources/views/cities/index.blade.php
@@ -3,46 +3,72 @@
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <div class="index-header border border-gray-200 p-6 rounded-xl">
-                        <h2>Cities</h2>
-                        <button href="/cities/create" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="create-city-modal">Create City</button>
-                    </div>
+                    <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">Cities</h2>
+                    @include ('cities._add-city-form')
                     @if ($cities->count())
                         <table class="min-w-full divide-y divide-gray-200">
-                            <tbody class="bg-white divide-y divide-gray-200">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody id="cities" class="bg-white divide-y divide-gray-200">
                                 @foreach ($cities as $city)
-                                    <tr>
+                                    <tr class="city">
                                         <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center">
-                                                <div class="text-sm font-medium text-gray-900">
-                                        <td>{{ $city->name }}</td>
+                                            <div>
+                                                <div class="text-sm font-medium">
+                                        <td>{{ $city->id }}</td>
                 </div>
             </div>
             </td>
-
-            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="/cities/{{ $city->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
-            </td>
-
-            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <form method="POST" action="/cities/city/{{ $city->id }}">
-                    @csrf
-                    @method('DELETE')
-
-                    <button class="text-xs text-gray-400">Delete</button>
-                </form>
-            </td>
-            </tr>
-            @endforeach
-            </tbody>
-            </table>
-        @else
-            <p class="text-center">No cities yet.</p>
-            @endif
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div>
+                    <div class="text-sm font-medium text-gray-900">
+            <td>{{ $city->name }}</td>
         </div>
     </div>
+    </td>
+
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+        <a href="/cities/{{ $city->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
+    </td>
+
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+        <form class="delete-city-form" method="DELETE" action="/cities/{{ $city->id }}">
+            @csrf
+            @method('DELETE')
+            <button class="text-xs text-gray-400">Delete</button>
+        </form>
+    </td>
+    </tr>
+    @endforeach
+    </tbody>
+    </table>
+@else
+    <p class="text-center">No cities yet.</p>
+    @endif
     </div>
     </div>
-@include ('cities._add-city-form')
+    </div>
+    </div>
+
+    <script>
+        $('document').ready(() => {
+            $(".delete-city-form").submit(function(e){
+                formRequest({
+                    e,
+                    form: $(this),
+                    success: () => {
+                        alert('city deleted successfully');
+                        $(this).closest('.city').remove();
+                    }
+                });
+            });
+        });
+    </script>
 
 </x-layout>
