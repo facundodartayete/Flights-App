80e702e07f94a1fee41d12c05a63d20787213a41
Facundo <facundodartayete@gmail.com>
2022-03-01 09:32:52 -0300

    flight delete

--- flights-app/resources/js/app.js
+++ flights-app/resources/js/app.js
@@ -1,10 +1,28 @@
-require('./bootstrap');
+require("./bootstrap");
 
-import {createApp} from 'vue'
-import Flights from './components/Flights'
+import { createApp } from "vue";
+import Flights from "./components/Flights";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-createApp({
-    components:{
+const app = createApp({
+    components: {
         Flights,
-    }
-}).mount('#app');
+    },
+});
+
+app.use(Toast, {
+    position: "top-right",
+    timeout: 3000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: false,
+    draggable: false,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+});
+
+app.mount("#app");
--- flights-app/routes/api.php
+++ flights-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AirlineController;
+use App\Http\Controllers\FlightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/airlines', [AirlineController::class, 'getAirlines']);
+
+
+Route::delete('/flights/{flight}', [FlightController::class, 'delete']);
+Route::get('/flights', [FlightController::class, 'getFlights']);
