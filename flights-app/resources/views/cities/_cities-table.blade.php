e34cd54ba59fca2d916ec184e08af90c9dda119b
Facundo <facundodartayete@gmail.com>
2022-03-02 15:38:25 -0300

    cities refactoring

--- flights-app/resources/views/cities/_cities-table.blade.php
+++ flights-app/resources/views/cities/_cities-table.blade.php
@@ -1,26 +1,59 @@
-<div id="cities-table" class="table-wrapper">
-    <table class="table-fixed">
-        <thead>
-            <tr>
-                <td>Id</td>
-                <td>Name</td>
-                <td>Departures</td>
-                <td>Arrivals</td>
-                <td></td>
-                <td></td>
-            </tr>
-        </thead>
-        <tbody id="cities">
-            @foreach ($cities as $city)
-                @include ('cities._city-row')
-            @endforeach
-        </tbody>
-    </table>
+<div id="cities-table">
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full">
+                        <thead class="bg-slate-100 border-b">
+                            <tr>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Id
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Name
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Departures
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                >
+                                    Arrivals
+                                </th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                ></th>
+                                <th
+                                    scope="col"
+                                    class="text-sm font-bold text-gray-900 px-6 py-4 text-left"
+                                ></th>
+                            </tr>
+                        </thead>
+                        <tbody id="cities">
+                            @foreach ($cities as $city) @include ('cities._city-row')
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
     {{ $cities->links() }}
 
     <script>
         $(document).ready(() => {
-            $(".delete-city-form").submit(function(e) {
+            $(".delete-city-form").submit(function (e) {
                 formRequest({
                     e,
                     form: $(this),
@@ -30,7 +63,7 @@
                 });
             });
 
-            $(".edit-city").on("click", function() {
+            $(".edit-city").on("click", function () {
                 let city = $(this).closest(".city");
                 $("#edit-city-modal").trigger("activate", {
                     cityId: city.data("city-id"),
--- flights-app/resources/views/cities/_city-row.blade.php
+++ flights-app/resources/views/cities/_city-row.blade.php
@@ -1,27 +1,36 @@
-<tr class="city" data-city-id="{{$city->id}}">
-    <td>{{ $city->id }}</td>
-    <td class="city-name">
+<tr
+    class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 city"
+    data-city-id="{{$city->id}}"
+>
+    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+        {{ $city->id }}
+    </td>
+    <td
+        class="city-name text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
         {{ $city->name }}
     </td>
-    <td>
+    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
         {{ $city->flights_departing_count }}
     </td>
-    <td>
+    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
         {{ $city->flights_arriving_count }}
     </td>
 
-    <td>
-        <div class="edit-city text-blue-500 hover:text-blue-600">Edit</div>
+    <td
+        class="edit-city text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-indigo-600 font-bold cursor-pointer"
+    >
+        Edit
     </td>
 
-    <td>
+    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap text-red-600 font-bold cursor-pointer">
         <form
             class="delete-city-form"
             method="DELETE"
             action="/cities/{{ $city->id }}"
         >
             @csrf @method('DELETE')
-            <button class="text-xs text-gray-400">Delete</button>
+            <button class="text-xs ">Delete</button>
         </form>
     </td>
 </tr>
