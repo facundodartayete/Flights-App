a976758db9f20929d054911df5db5666f7a30bb3
Facundo <facundodartayete@gmail.com>
2022-03-04 11:40:24 -0300

    flight controller use tap instead of save

--- flights-app/app/Http/Controllers/FlightController.php
+++ flights-app/app/Http/Controllers/FlightController.php
@@ -27,13 +27,13 @@ class FlightController extends Controller
     public function update(Flight $flight)
     {
         $flightRequest = $this->validateFlight($flight);
-        $flight->airline_id = $flightRequest['airline_id'];
-        $flight->origin_city_id = $flightRequest['origin_city_id'];
-        $flight->destination_city_id = $flightRequest['destination_city_id'];
-        $flight->departure_at = $flightRequest['departure_at'];
-        $flight->arrival_at = $flightRequest['arrival_at'];
-        $flight->save();
-        return ['flight' => $flight];
+        return tap($flight)->update([
+            'airline_id' => $flightRequest['airline_id'],
+            'origin_city_id' => $flightRequest['origin_city_id'],
+            'destination_city_id' => $flightRequest['destination_city_id'],
+            'departure_at' => $flightRequest['departure_at'],
+            'arrival_at' => $flightRequest['arrival_at'],
+        ]);
     }
 
     protected function validateFlight(?Flight $flight = null): array
